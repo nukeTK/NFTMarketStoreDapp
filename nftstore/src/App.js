2d89e168558e8a2533220984a0e5517622256fde
nukeTK <tarandeespsingh1809@gmail.com>
2022-08-09 01:52:25 +0530

    first commit

--- nftstore/src/App.js
+++ nftstore/src/App.js
@@ -73,7 +73,7 @@ const App = () => {
   }, []);
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/NFTMarketStoreDapp">
       {chainID && <NavBar account={account} connect={web3Connection} />}
       <Routes>
         <Route
